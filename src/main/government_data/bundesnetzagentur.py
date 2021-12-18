5e201dee62ed874a36d16be12ba30b2a3126aee6
michael <icemikl7@gmail.com>
2021-12-18 18:35:48 +0100

    - Refactored the scrapper.

--- src/main/government_data/bundesnetzagentur.py
+++ src/main/government_data/bundesnetzagentur.py
@@ -1,33 +1,18 @@
-import requests
 
+import json
 import scrapy
 from scrapy.crawler import CrawlerProcess
-
 from datetime import date
-
 import re
 
 
-
-class Bundesnetzagentur:
-
-    def __init__(self):
-        pass
-
-
-    def getBlockedNumbers(self):
-        response = requests.get("https://www.bundesnetzagentur.de/DE/Vportal/TK/Aerger/Aktuelles/start.html#AnkerMassnahmen")
-        print(response.status_code)
-        print(response.content)
-
-
-
-class QuotesSpider(scrapy.Spider):
+class Bundesnetzagentur(scrapy.Spider):
 
     name = "bundesnetzagentur"
     custom_settings = {
-        'DOWNLOD_DELAY': 1
+        'DOWNLOAD_DELAY': 1
     }
+
 
     def start_requests(self):
         urls = [
@@ -36,37 +21,40 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+
     def parse(self, response):
-        current_date = self.get_current_date_as_string()
-        filename = f'bundesnetzagentur_{current_date}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        new_data = []
 
         table_with_numbers = response.css('#content table')
         rows = table_with_numbers.xpath('./tbody/tr')
 
         for row in rows:
-            print('')
+            number_object = {}
             for i, value in enumerate(row.xpath('./td')):
                 if('xl' in value.xpath('./@class').get()):
                     if(i == 0):
-                        date = value.xpath('./text()').get()
-                        print(f'date: {date}')
+                        number_object['date'] = value.xpath('./text()').get()
                     elif(i == 1):
                         text_value = value.xpath('./text()').get()
                         if self.is_a_list_of_numbers(text_value):
-                            numbers_list = text_value.replace(' ', '').split(',')
+                            number_object['numbers_list'] = text_value.replace(' ', '').split(',')
                         else:
-                            numbers_list = [text_value]
-                        print(f'numbers: {str(numbers_list)}')
+                            number_object['numbers_list'] = [text_value]
                     elif(i == 2):
-                        category = value.xpath('./text()').get()
-                        print(f'category: {category}')
+                        number_object['category'] = value.xpath('./text()').get()
                     elif(i == 3):
-                        action = value.xpath('./text()').get()
-                        print(f'action: {action}')
+                        number_object['action'] = value.xpath('./text()').get()
+            if number_object != {}:
+                new_data.append(number_object)
 
+        self.write_new_data_to_file(new_data=new_data)
+
+
+    def write_new_data_to_file(self, new_data):
+        current_date = self.get_current_date_as_string()
+        filename = f'resources/bundesnetzagentur_{current_date}.json'
+        with open(filename, 'wb') as f:
+            f.write(json.dumps(new_data, indent=2).encode('utf-8'))
 
 
     def get_current_date_as_string(self):
@@ -77,11 +65,11 @@ class QuotesSpider(scrapy.Spider):
         pattern = re.compile("([0-9]+((,)\s*)?)+")
         return pattern.match(value)
 
-if __name__ == "__main__":
-    #bundesnetzagentur = Bundesnetzagentur()
-    #bundesnetzagentur.getBlockedNumbers()
 
+if __name__ == "__main__":
     process = CrawlerProcess()
-    process.crawl(QuotesSpider)
+    process.crawl(Bundesnetzagentur)
     process.start()
+
+
 
