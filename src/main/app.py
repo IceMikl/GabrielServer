29e9407ec7075ba4a49197a72e9e5fbbe0f663d8
michael <icemikl7@gmail.com>
2021-12-19 19:01:55 +0100

    - Initial commit

--- src/main/app.py
+++ src/main/app.py
@@ -12,6 +12,17 @@ import src.main.database.database_manager as db_manager
 
 from config import config
 
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+        "dbname='template1'"
+        " user='dbuser'"
+        " host='localhost'"
+        " password='dbpass'")
+except:
+    print("I am unable to connect to the database")
+
 
 app = Flask(__name__)
 api = Api(app)
