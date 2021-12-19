80671af04733ffc4381bfffa2d97b16c007169cf
michael <icemikl7@gmail.com>
2021-12-19 11:05:28 +0100

    - Do not need here anymore.

--- createContainer.bat
+++
@@ -1,6 +0,0 @@
-
-CALL docker build -t server_image .
-CALL docker container stop server_container
-CALL docker rm server_container
-CALL docker create --name server_container --publish 8080:8080 server_image
-CALL docker start server_container -a
