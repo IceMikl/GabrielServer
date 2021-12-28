14422f25bb038de344278f4bb208bd831047ee61
z004564k <mikhail.erushev@siemens.com>
2021-12-28 21:46:01 +0100

    - Implemented a database model for given numbers.

--- src/main/database/models.py
+++ src/main/database/models.py
@@ -3,8 +3,8 @@ from sqlalchemy import Column, Integer, String, Sequence
 
 
 class BaseModel:
-
     BASE = declarative_base()
+
 
 
 class BlockedNumber(BaseModel.BASE):
@@ -18,6 +18,21 @@ class BlockedNumber(BaseModel.BASE):
     def __repr__(self):
         return f'<BlockedNumber(id={self.id}, phone_number={self.phone_number}, ' \
                f'description={self.description}, suspicious={self.suspicious})>'
+
+
+
+class GivenNumber(BaseModel.BASE):
+    __tablename__ = 'GivenNumber'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    phone_number = Column(String)
+    area_code = Column(String)
+    place_name = Column(String)
+    phone_provider = Column(String)
+
+    def __repr__(self):
+        return f'<GivenNumber(id={self.id}, phone_number={self.phone_number}, area_code={self.area_code}, ' \
+               f'place_name={self.place_name}, phone_provider={self.phone_provider})>'
 
 
 
