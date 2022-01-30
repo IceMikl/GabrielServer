08539afb902e0c3ecb1cdfd83afc04d68223b231
IceMikl <icemikl7@gmail.com>
2022-01-30 20:00:29 +0100

    - Removed unused file.

--- createContainer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker build -t multiple_containers_server_image .
-sudo docker container stop icemikl/multiple_containers_server
-sudo docker rm icemikl/multiple_containers_server
-sudo docker create --name icemikl/multiple_containers_server --publish 8080:8080 multiple_containers_server_image
-sudo docker start icemikl/multiple_containers_server -a
