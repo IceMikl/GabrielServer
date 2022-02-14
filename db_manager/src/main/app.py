39eff7bbff4809eb53faa98a70dc3026947281a4
IceMikl <icemikl7@gmail.com>
2022-02-14 09:35:25 +0100

    - Resolved bugs in build.

--- db_manager/src/main/app.py
+++ db_manager/src/main/app.py
@@ -2,6 +2,7 @@ import sys
 
 if ("/" not in sys.path):
     sys.path.append("/")
+    sys.path.append("/db_manager")
 print(sys.path)
 
 from db_manager.src.main.server.server import Server
--- request_handler/src/main/app.py
+++ request_handler/src/main/app.py
@@ -2,6 +2,7 @@ import sys
 
 if("/" not in sys.path):
     sys.path.append("/")
+    sys.path.append("/request_handler")
 print(sys.path)
 
 from request_handler.src.main.server.server import Server
